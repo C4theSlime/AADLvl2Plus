fb7c899266a2c1bb6db35f11adc2ccf72216f9d7
Maarten Neesen <c4theslime@gmail.com>
2020-10-19 12:34:04 +0200

    Enabled view binding and added functionality to QuestionAdapter, aswell as added initial questions to the Question data class

--- MadLevel2Task2/app/src/main/java/com/example/madlevel2task2/Question.kt
+++ MadLevel2Task2/app/src/main/java/com/example/madlevel2task2/Question.kt
@@ -2,4 +2,12 @@ package com.example.madlevel2task2
 
 data class Question (
     var questionText: String
-)
+) {
+    companion object {
+        val QUESTIONS = arrayOf(
+            "Using an AND gate with the inputs T and F returns True.",
+            "Val in Kotlin acts like a 'final' variable in other languages",
+            "1 + 1 = 3"
+        )
+    }
+}
