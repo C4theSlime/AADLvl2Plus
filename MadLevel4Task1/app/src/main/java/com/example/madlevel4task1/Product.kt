eabff869f2a920368f8a2ae697618f1e38aff7b1
Maarten Neesen <c4theslime@gmail.com>
2020-10-20 16:02:34 +0200

    Added base classes for Room database to function

--- MadLevel4Task1/app/src/main/java/com/example/madlevel4task1/Product.kt
+++ MadLevel4Task1/app/src/main/java/com/example/madlevel4task1/Product.kt
@@ -1,6 +1,16 @@
 package com.example.madlevel4task1
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "product_table")
 data class Product (
-    var productName : String,
-    var productAmount: Short
+    @ColumnInfo(name = "productAmount") var productAmount: Short,
+    @ColumnInfo(name = "productName") var productName : String,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Long? = null
+
 )
